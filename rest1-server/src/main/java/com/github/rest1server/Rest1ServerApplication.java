5616200bca8258ec3de9c29078e4f5a45c0f10b4
batiwo <sebgwada712@gmail.com>
2018-07-15 11:43:16 +0200

    Formatting code

--- rest1-server/src/main/java/com/github/rest1server/Rest1ServerApplication.java
+++ rest1-server/src/main/java/com/github/rest1server/Rest1ServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Rest1ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Rest1ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Rest1ServerApplication.class, args);
+    }
 }
--- zuul-server/src/test/java/com/github/zuulserver/ZuulServerApplicationTests.java
+++ zuul-server/src/test/java/com/github/zuulserver/ZuulServerApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ZuulServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
